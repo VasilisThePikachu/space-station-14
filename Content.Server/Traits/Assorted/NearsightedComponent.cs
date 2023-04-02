34a5b7bfd5477a821021e37ee3bc1ef77fc7d810
Vasilis The Pikachu <vasilis@pikachu.systems>
2023-04-02 22:26:38 +0200

    note to self: rider smart shelf is goofy. Better to push

---
+++ Content.Server/Traits/Assorted/NearsightedComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server.Traits.Assorted;
+
+/// <summary>
+/// This is used for Nearsightedness.
+/// </summary>
+[RegisterComponent]
+public sealed class NearsightedComponent : Component
+{
+
+}
--- Content.Server/Traits/Assorted/NearsightedSystem.cs
+++ Content.Server/Traits/Assorted/NearsightedSystem.cs
@@ -1,8 +1,23 @@
-﻿using Content.Shared.Traits.Assorted;
+﻿using Content.Server.GameTicking;
+using Content.Shared.Traits.Assorted;
 
 namespace Content.Server.Traits.Assorted;
 
 public sealed class NearsightedSystem : SharedNearsightedSystem
 {
+    public override void Initialize()
+    {
+        base.Initialize();
 
+        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawn);
+    }
+
+
+    private void OnPlayerSpawn(PlayerSpawnCompleteEvent args)
+    {
+        foreach (var comp in EntityQuery<NearsightedComponent>())
+        {
+
+        }
+    }
 }
